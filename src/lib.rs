abcdb2a44ff3b6198291cfac109b18e3791a2a15
Dakota <72140289+1337Dakota@users.noreply.github.com>
2024-12-15 15:31:25 +0100

    Updated methods to return index

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 //! A simple binary pattern matching library
 //!
+//!
 //! Basic Usage looks like this:
 //! ```
 //! # use binmatch::Pattern;
 //! let pattern = Pattern::new("00 00 ??").unwrap();
 //! let data = vec![0x12, 0x13, 0x14, 0x00, 0x00, 0x42, 0x15];
-//! let matches = pattern.find_matches(data);
+//! let matches = pattern.find_matches(data); // Or Pattern::find_matches_with_index if you need the index
 //! assert_eq!(matches, vec![0x42]);
 //! ```
 //!
@@ -41,13 +42,13 @@ impl fmt::Display for BinmatchError {
 }
 impl std::error::Error for BinmatchError {}
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
 pub struct Pattern {
     data: Vec<PatternElement>,
     len: usize,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 enum PatternElement {
     Literal(u8),
     Placeholder,
@@ -88,6 +89,31 @@ impl Pattern {
 
     /// Finds all matches in the `haystack`
     ///
+    /// Use [Pattern::find_matches] if you don't need the index
+    ///
+    /// # Example:
+    /// ```
+    /// # use binmatch::Pattern;
+    /// let pattern = Pattern::new("00 00 ??").unwrap();
+    /// let data = vec![0xFF, 0x12, 0x34, 0x00, 0x00, 0x42, 0x56, 0x78];
+    /// let matches = pattern.find_matches_with_index(data);
+    /// assert_eq!(matches, vec![(0x42, 5)]);
+    /// ```
+    pub fn find_matches_with_index(&self, haystack: Vec<u8>) -> Vec<(u8, usize)> {
+        let mut matches = vec![];
+        for (i, sub) in haystack.windows(self.len).enumerate() {
+            matches.extend(
+                self.match_chunk(sub.to_vec())
+                    .iter()
+                    .map(|m| (m.0, m.1 + i))
+                    .collect::<Vec<(u8, usize)>>(),
+            );
+        }
+        matches
+    }
+
+    /// Convenience Method for cases where the index is not needed
+    ///
     /// # Example:
     /// ```
     /// # use binmatch::Pattern;
@@ -97,46 +123,53 @@ impl Pattern {
     /// assert_eq!(matches, vec![0x42]);
     /// ```
     pub fn find_matches(&self, haystack: Vec<u8>) -> Vec<u8> {
-        let mut matches = vec![];
-        for sub in haystack.windows(self.len) {
-            matches.extend(self.match_chunk(sub.to_vec()));
-        }
-        matches
+        let matches = self.find_matches_with_index(haystack);
+        matches.iter().map(|(matched, _)| *matched).collect()
     }
 
-    /// Finds a match in a chunk
-    /// Called by [Pattern::find_matches]
+    /// Finds a match in a chunk  
+    /// Called by [Pattern::find_matches]  
+    /// You normally don't need to use this
     ///
-    /// Pattern.len **MUST** be the same size as chunk.len
+    /// `Pattern.len()` **MUST** be the same size as `chunk.len()`
+    ///
+    /// Returns a Vec of Tuples of the matched Value and the Index
     ///
     /// # Examples:
     /// ```
     /// # use binmatch::Pattern;
     /// let pattern = Pattern::new("00 00 ??").unwrap();
     /// let matches = pattern.match_chunk(vec![0x00, 0x00, 0x42]);
-    /// assert_eq!(matches, vec![0x42]);
+    /// assert_eq!(matches, vec![(0x42, 2)]);
     /// ```
     ///
     /// ```should_panic
     /// # use binmatch::Pattern;
-    /// // This will panic
     /// let pattern = Pattern::new("00 00 ??").unwrap();
     /// let matches = pattern.match_chunk(vec![0x00, 0x00, 0x42, 0x00]);
     /// unreachable!();
     /// ```
-    pub fn match_chunk(&self, chunk: Vec<u8>) -> Vec<u8> {
+    pub fn match_chunk(&self, chunk: Vec<u8>) -> Vec<(u8, usize)> {
         assert_eq!(self.len, chunk.len());
         let mut matches = vec![];
-        for (actual, expected) in chunk.iter().zip(self.data.clone()) {
+        for (index, (actual, expected)) in chunk.iter().zip(self.data.clone()).enumerate() {
             match expected {
                 PatternElement::Literal(expected) => {
                     if expected != *actual {
                         return vec![]; // Discard all matches
                     }
                 }
-                PatternElement::Placeholder => matches.push(*actual),
+                PatternElement::Placeholder => matches.push((*actual, index)),
             }
         }
         matches
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -4,11 +4,25 @@ use crate::*;
 fn test_pattern_chunk_matching() {
     let pattern = Pattern::new("00 ?? 00 ??").unwrap();
     let matches = pattern.match_chunk(vec![0, 42, 0, 13]);
-    assert_eq!(matches, vec![42, 13])
+    assert_eq!(matches, vec![(42, 1), (13, 3)])
 }
 
 #[test]
 fn test_pattern_whole_matching() {
+    let pattern = Pattern::new("DEADBEEF ??").unwrap();
+    let mut data: Vec<u8> = vec![0xDE, 0xAD, 0xBE, 0xEF, 23];
+    for i in 0..100 {
+        data.insert(0, i);
+    }
+    for i in 0..100 {
+        data.push(i);
+    }
+    let matches = pattern.find_matches_with_index(data);
+    assert_eq!(matches, vec![(23, 100 + 4)]);
+}
+
+#[test]
+fn test_convinience_method() {
     let pattern = Pattern::new("DEADBEEF ??").unwrap();
     let mut data: Vec<u8> = vec![0xDE, 0xAD, 0xBE, 0xEF, 23];
     for i in 0..100 {
